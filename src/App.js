c4d044731c4804e25c4397b92c06c1c167eec493
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-12-17 10:39:00 -0500

    Front End Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StripeCheckout from 'react-stripe-checkout'
 
 class App extends Component {
+
+  onToken(token){
+    console.log("Clicked", token)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <StripeCheckout
+        token={this.onToken}
+        stripeKey="pk_test_ruJP16BdItLFkRcf86KGQYvP"
+        />
       </div>
     );
   }
